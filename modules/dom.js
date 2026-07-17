98942e429d58fcf01f0bd8e0e4f732c859b3ffd5
Kurniawan Syafitra <129821438+Kursyiandi@users.noreply.github.com>
2026-07-17 14:10:41 +0700

    Update dom.js

--- modules/dom.js
+++ modules/dom.js
@@ -1,45 +1,42 @@
-// Memetakan semua elemen HTML ke dalam satu objek
 export const el = {
-    // 1. Area Utama & Navigasi
     areaLogin: document.getElementById('areaLogin'),
     areaApp: document.getElementById('areaApp'),
     areaUtama: document.getElementById('areaUtama'),
     areaBulanan: document.getElementById('areaBulanan'),
     infoUser: document.getElementById('infoUser'),
-    teksTanggalHeader: document.getElementById('teksTanggalHeader'),
-    
-    // 2. Tombol Utama
+    overlayModal: document.getElementById('overlayModal'),
+    toastNotification: document.getElementById('toastNotification'),
+    daftarItem: document.getElementById('daftarItem'),
+
     btnLogin: document.getElementById('btnLogin'),
     btnLogout: document.getElementById('btnLogout'),
-    
-    // 3. Tab Menu & Halaman
-    btnMenuCatatan: document.getElementById('btnMenuCatatan'),
-    btnMenuPengeluaran: document.getElementById('btnMenuPengeluaran'),
-    btnMenuAkun: document.getElementById('btnMenuAkun'),
-    halamanCatatan: document.getElementById('halamanCatatan'),
-    halamanPengeluaran: document.getElementById('halamanPengeluaran'),
-    halamanAkun: document.getElementById('halamanAkun'),
-    
-    // 4. Kontrol Waktu & Pencarian
+
+    teksTanggalHeader: document.getElementById('teksTanggalHeader'),
     wadahNavTanggal: document.getElementById('wadahNavTanggal'),
     btnPrevTanggal: document.getElementById('btnPrevTanggal'),
     btnNextTanggal: document.getElementById('btnNextTanggal'),
-    inputTanggal: document.getElementById('inputTanggalPilih'), 
+    inputTanggal: document.getElementById('inputTanggalPilih'),
     wadahCari: document.getElementById('wadahCari'),
     inputCari: document.getElementById('inputCariItem'),
-    btnBukaSebulan: document.getElementById('btnBukaSebulan'),
+
+    btnMenuCatatan: document.getElementById('btnMenuCatatan'),
+    btnMenuPengeluaran: document.getElementById('btnMenuPengeluaran'),
+    btnMenuAkun: document.getElementById('btnMenuAkun'),
     
-    // 5. Form Catatan
+    halamanCatatan: document.getElementById('halamanCatatan'),
+    halamanPengeluaran: document.getElementById('halamanPengeluaran'),
+    halamanAkun: document.getElementById('halamanAkun'),
+
     btnTambahCatatan: document.getElementById('btnTambahCatatan'),
     wadahInputCatatan: document.getElementById('wadahInputCatatan'),
     inputCatatan: document.getElementById('inputCatatan'),
     btnBatalCatatan: document.getElementById('btnBatalCatatan'),
     btnSimpanCatatan: document.getElementById('btnSimpanCatatan'),
     btnSuaraCatatan: document.getElementById('btnSuaraCatatan'),
-    
-    // 6. Form Pengeluaran
+
     angkaTotalHari: document.getElementById('angkaTotalHari'),
     angkaTotalBulan: document.getElementById('angkaTotalBulan'),
+    boxTotalBulan: document.getElementById('boxTotalBulan'),
     btnTambahPengeluaran: document.getElementById('btnTambahPengeluaran'),
     wadahInputPengeluaran: document.getElementById('wadahInputPengeluaran'),
     inputNamaPengeluaran: document.getElementById('inputNamaPengeluaran'),
@@ -47,9 +44,8 @@ export const el = {
     inputKategoriPengeluaran: document.getElementById('inputKategoriPengeluaran'),
     btnBatalPengeluaran: document.getElementById('btnBatalPengeluaran'),
     btnSimpanPengeluaran: document.getElementById('btnSimpanPengeluaran'),
-    boxTotalBulan: document.getElementById('boxTotalBulan'),
-    
-    // 7. Form Akun
+    btnBukaSebulan: document.getElementById('btnBukaSebulan'),
+
     inputPinMaster: document.getElementById('inputPinMaster'),
     btnTambahAkun: document.getElementById('btnTambahAkun'),
     wadahInputAkun: document.getElementById('wadahInputAkun'),
@@ -57,30 +53,26 @@ export const el = {
     inputUsername: document.getElementById('inputUsername'),
     inputPasswordAkun: document.getElementById('inputPasswordAkun'),
     btnBatalAkun: document.getElementById('btnBatalAkun'),
-    grafikKategori: document.getElementById('grafikKategori'),
     btnSimpanAkun: document.getElementById('btnSimpanAkun'),
-    
-// 8. Daftar & Modal Edit
-    daftarItem: document.getElementById('daftarItem'),
-    overlayModal: document.getElementById('overlayModal'),
+
     modalEdit: document.getElementById('modalEdit'),
     inputEditCatatan: document.getElementById('inputEditCatatan'),
     btnBatalEdit: document.getElementById('btnBatalEdit'),
     btnSimpanEdit: document.getElementById('btnSimpanEdit'),
+    
     modalEditPengeluaran: document.getElementById('modalEditPengeluaran'),
     inputEditNamaPengeluaran: document.getElementById('inputEditNamaPengeluaran'),
     inputEditNominalPengeluaran: document.getElementById('inputEditNominalPengeluaran'),
     inputEditKategoriPengeluaran: document.getElementById('inputEditKategoriPengeluaran'),
     btnBatalEditPengeluaran: document.getElementById('btnBatalEditPengeluaran'),
     btnSimpanEditPengeluaran: document.getElementById('btnSimpanEditPengeluaran'),
-    
-    // 9. Komponen Lainnya
-    toastNotification: document.getElementById('toastNotification'),
+
     btnKembali: document.getElementById('btnKembali'),
     judulBulanan: document.getElementById('judulBulanan'),
     btnDownloadPDF: document.getElementById('btnDownloadPDF'),
     inputBulanRekap: document.getElementById('inputBulanRekap'),
     wadahGrafik: document.getElementById('wadahGrafik'),
     grafikPengeluaran: document.getElementById('grafikPengeluaran'),
+    grafikKategori: document.getElementById('grafikKategori'),
     kontenBulanan: document.getElementById('kontenBulanan')
 };
